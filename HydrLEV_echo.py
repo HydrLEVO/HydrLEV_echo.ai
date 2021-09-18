f100fdcae19c608357f0fa30e7878c17a6a458b9
LEVO <90807226+HydrLEVO@users.noreply.github.com>
2021-09-18 20:36:27 +1000

    Add files via upload

---
+++ HydrLEV_echo.py
@@ -0,0 +1,109 @@
+import time 
+import os
+import sys
+import platform
+
+
+print("""
+
+                                          🇭​​​​​   🇾   ​​​​​🇩​​​​​   🇷​​​​​   🇦​​​​​       🇪​​​​​   🇨​​​​​   🇭​​​​​   🇴​​​​​
+                                                      the right AI for a family
+""")
+print("hello, my name is hydra echo. an AI created by LEVO. how can i help")
+select = input("T> ")
+
+if select == 'hello':
+  print("hello to you back")
+select = input("T> ")
+ 
+if select == 'help':
+  print("what do you need help with")
+select = input("T> ")
+
+if select == 'with cmds':
+      print("here are all cmds>>>  hello,how are you,who are you, who is the creater of this AI, date")
+select = input("T> ")
+  
+if select == 'date':
+      print("the date is: " + time.strftime("%m/%d/%y"))
+select = input("T> ") 
+ 
+if select == 'how are you':
+      print("im good thanks, how are you?")
+select = input("T> ")  
+  
+if select == 'good':
+      print("good")
+select = input("T> ")     
+
+if select == 'who are you':
+      print("im an AI for familys and made from the owner of LEVO")
+select = input("T> ")  
+if select == 'who if the creater of this AI':
+      print("LEVO and Hydra")
+select = input("T> ") 
+  
+if code == 'dictionary':
+      print("sorry, coming soon")
+select = input("T> ") 
+  
+if select == 'game':
+      print("okay, number 1 or 2?")
+select = input("T> ")
+
+if select == '1':
+     print("i have a tail and im a primate, what am i")
+select = input("T> ")
+
+if select == 'monkey': 
+     print("correct")
+select = input("T> ")
+if select == '2':
+     print("i have 8 legs and 7 eyes, what am i")
+select = input("T> ")     
+     
+if select == 'spider':
+       print("correct")
+select = input("T> ")
+       
+if select == 'game2':
+       print("okay, ready for game2. >> type > E or H")
+select = input("T> ")  
+  
+if select == 'e':
+        print("10x10= ?")
+select = input("T> ")    
+    
+if select == '100':
+         print("NICE")
+select = input("T> ")    
+   
+if select == 'h':
+           print("1̷̖̯̻͍̱͓͈̼͂̍2̸͈̖͈̼̏̐͠3̶͍̥͊̂͐̌́̊̋̌̚1̷̛͙͙̻̑̌̋̌̀̋2̶̛̤̗̫̹̒͗̀̾͗͋̈́̄͘3̵̞̦͕͓̩̺̊͗̒-̴̼̉́̎̎1̸̖̤͕͇̀́̔̆̌̈́́͛͐͝2̶̤͔͎̽̅͑͝͝3̴̳̜̠̌̇̇̓̃̂͘͝1̷̡̮̞̬͓̤͉̲̞͠2̸̢͚̗͙̠̜̫̥͗̽͠3̵̣͖͖̩͉̃̓̓̈́͝=̵̱̖̭̘̉̋͛́̆̕͘͜")
+select = input("T> ")    
+    
+if select == '0':
+           print("legendary!")
+select = input("T> ")      
+      
+if select == 'tell me a joke':
+  if select == 'joke':
+           print("knok knok.")
+select = input("T> ")
+
+if select == 'whos there?':
+   if select == 'whos there':
+            print("doctor")
+select = input("T> ")       
+   
+if select == 'doctor who?':
+    if select == 'doctor who':
+    print("hahaha, made you say doctor who")
+select = input("T> ")           
+           
+if select == 'quit':
+    print("are you sure you want to quit? y/n")
+select = input("T> ")    
+    
+if select == 'y':
+      os.system('Exit')
